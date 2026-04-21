ea90a976e58bc52aee0c7717b5ef3e2e9a38a050
zgagam <121051326+zgagam@users.noreply.github.com>
2026-04-21 08:54:42 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'bedek-v1';
+const FILES = [
+  '/Bedek/',
+  '/Bedek/index.html',
+  '/Bedek/manifest.json',
+  '/Bedek/icon-192.png',
+  '/Bedek/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/Bedek/index.html')))
+  );
+});
